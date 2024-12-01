226f95bfc18b9241c456f51fd05f28ef28647482
Fernando Fonseca <54486077+FernandoWF@users.noreply.github.com>
2024-12-01 03:38:57 -0300

    Add day 1 solution

--- Day01/Solution.cs
+++ Day01/Solution.cs
@@ -4,11 +4,41 @@ internal sealed class Solution : ISolution
 {
     public static object RunPart1(Input input)
     {
-        return string.Empty;
+        (var leftIds, var rightIds) = ParseIds(input);
+        leftIds.Sort();
+        rightIds.Sort();
+
+        return leftIds
+            .Zip(rightIds)
+            .Select(tuple => Math.Abs(tuple.First - tuple.Second))
+            .Sum();
     }
 
     public static object RunPart2(Input input)
     {
-        return string.Empty;
+        (var leftIds, var rightIds) = ParseIds(input);
+
+        return leftIds
+            .Select(leftId => leftId * rightIds.Count(rightId => rightId == leftId))
+            .Sum();
+    }
+
+    private static (List<int> leftIds, List<int> rightIds) ParseIds(Input input)
+    {
+        var leftIds = new List<int>();
+        var rightIds = new List<int>();
+
+        const int IdLength = 5;
+        const int SeparationLength = 3;
+
+        foreach (var line in input.Lines)
+        {
+            var leftId = int.Parse(line[..IdLength]);
+            leftIds.Add(leftId);
+            var rightId = int.Parse(line[(IdLength + SeparationLength)..]);
+            rightIds.Add(rightId);
+        }
+
+        return (leftIds, rightIds);
     }
 }
