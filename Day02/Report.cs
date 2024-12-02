2db6a037457403b9c72a19a6b1fd995e42eb862f
Fernando Fonseca <54486077+FernandoWF@users.noreply.github.com>
2024-12-02 02:36:25 -0300

    Add day 2 solution

---
+++ Day02/Report.cs
@@ -0,0 +1,69 @@
+﻿namespace AdventOfCode2024.Day02
+{
+    internal class Report
+    {
+        public bool IsSafe { get; }
+        public bool IsSafeWhenDampened { get; }
+
+        public Report(List<int> levels)
+        {
+            IsSafeWhenDampened = IsSafe = CheckSafe(levels);
+
+            if (IsSafeWhenDampened)
+            {
+                return;
+            }
+
+            for (var i = 0; i < levels.Count; i++)
+            {
+                var dampenedLevels = levels.ToList();
+                dampenedLevels.RemoveAt(i);
+
+                if (CheckSafe(dampenedLevels))
+                {
+                    IsSafeWhenDampened = true;
+                    return;
+                }
+            }
+        }
+
+        private static bool CheckSafe(List<int> levels)
+        {
+            var increasing = true;
+            var decreasing = true;
+
+            const int MinimumDifference = 1;
+            const int MaximumDifference = 3;
+            var validDifference = true;
+
+            for (var i = 1; i < levels.Count; i++)
+            {
+                var previousLevel = levels[i - 1];
+                var currentLevel = levels[i];
+
+                if (currentLevel >= previousLevel)
+                {
+                    decreasing = false;
+                }
+
+                if (currentLevel <= previousLevel)
+                {
+                    increasing = false;
+                }
+
+                var difference = Math.Abs(currentLevel - previousLevel);
+                if (difference < MinimumDifference || difference > MaximumDifference)
+                {
+                    validDifference = false;
+                }
+
+                if ((!increasing && !decreasing) || !validDifference)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Day02/Solution.cs
@@ -0,0 +1,32 @@
+﻿namespace AdventOfCode2024.Day02;
+
+internal sealed class Solution : ISolution
+{
+    public static object RunPart1(Input input)
+    {
+        return input.Lines
+            .Select(l =>
+            {
+                var levels = l.Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(n => int.Parse(n))
+                    .ToList();
+                return new Report(levels);
+            })
+            .Where(r => r.IsSafe)
+            .Count();
+    }
+
+    public static object RunPart2(Input input)
+    {
+        return input.Lines
+            .Select(l =>
+            {
+                var levels = l.Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(n => int.Parse(n))
+                    .ToList();
+                return new Report(levels);
+            })
+            .Where(r => r.IsSafeWhenDampened)
+            .Count();
+    }
+}
