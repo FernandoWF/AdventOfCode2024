77208d5468c3933f42a81cb9c2a7b452b1372ffb
Fernando Fonseca <54486077+FernandoWF@users.noreply.github.com>
2024-12-07 03:19:10 -0300

    Add day 7 solution

---
+++ Day07/Equation.cs
@@ -0,0 +1,17 @@
+﻿namespace AdventOfCode2024.Day07
+{
+    internal class Equation
+    {
+        public long Result { get; }
+        public IReadOnlyList<int> Numbers { get; }
+
+        public Equation(string line)
+        {
+            var parts = line.Split(": ");
+            Result = long.Parse(parts[0]);
+            Numbers = parts[1].Split(' ')
+                .Select(int.Parse)
+                .ToList();
+        }
+    }
+}
---
+++ Day07/Solution.cs
@@ -0,0 +1,53 @@
+﻿namespace AdventOfCode2024.Day07;
+
+internal sealed class Solution : ISolution
+{
+    public static object RunPart1(Input input)
+    {
+        var equations = input.Lines
+            .Select(l => new Equation(l))
+            .ToList();
+
+        static bool IsEquationSolvable(IReadOnlyList<int> numbers, long currentResult, long targetResult, int index)
+        {
+            if (index == numbers.Count) { return currentResult == targetResult; }
+            if (currentResult > targetResult) { return false; }
+
+            if (IsEquationSolvable(numbers, currentResult + numbers[index], targetResult, index + 1)) { return true; }
+            if (IsEquationSolvable(numbers, currentResult * numbers[index], targetResult, index + 1)) { return true; }
+
+            return false;
+        }
+
+        return equations
+            .Where(e => IsEquationSolvable(e.Numbers, e.Numbers[0], e.Result, 1))
+            .Select(e => e.Result)
+            .Sum();
+    }
+
+    public static object RunPart2(Input input)
+    {
+        var equations = input.Lines
+            .Select(l => new Equation(l))
+            .ToList();
+
+        static bool IsEquationSolvableWithConcatenation(IReadOnlyList<int> numbers, long currentResult, long targetResult, int index)
+        {
+            if (index == numbers.Count) { return currentResult == targetResult; }
+            if (currentResult > targetResult) { return false; }
+
+            if (IsEquationSolvableWithConcatenation(numbers, currentResult + numbers[index], targetResult, index + 1)) { return true; }
+            if (IsEquationSolvableWithConcatenation(numbers, currentResult * numbers[index], targetResult, index + 1)) { return true; }
+
+            var concatenatedValue = long.Parse(currentResult.ToString() + numbers[index].ToString());
+            if (IsEquationSolvableWithConcatenation(numbers, concatenatedValue, targetResult, index + 1)) { return true; }
+
+            return false;
+        }
+
+        return equations
+            .Where(e => IsEquationSolvableWithConcatenation(e.Numbers, e.Numbers[0], e.Result, 1))
+            .Select(e => e.Result)
+            .Sum();
+    }
+}
